56b1e18ab571bf604f11cf4e8f0488560a6f38d6
Eric <86986539+EricS-1@users.noreply.github.com>
2024-06-20 11:08:29 -0400

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import itertools
-allFeatures = ['MSSubClass','LowQualFinSF','GrLivArea','HalfBath','KitchenAbvGr','Fireplaces','WoodDeckSF','OpenPorchSF','EnclosedPorch','3SsnPorch','ScreenPorch','OverallQual','OverallCond','YearRemodAdd','PoolArea','MiscVal','MoSold','YrSold']
-features = ['LotArea','YearBuilt','1stFlrSF','2ndFlrSF','FullBath','BedroomAbvGr','TotRmsAbvGrd']
-
-print(len(allFeatures))
-print(len(features))
